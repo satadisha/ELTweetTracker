6bf5bcb15ee9f7a082925655b9a4990c59857fed
satadisha <ssahabh1@binghamton.edu>
2017-03-19 23:51:43 -0400

    multiphase implementation: date and number handling fixed, running on new sample

--- ELTweetTracker/NE_candidate_module.py
+++ ELTweetTracker/NE_candidate_module.py
@@ -3,24 +3,35 @@ start_of_sentence=1
 abbreviation=2
 all_capitalized=3
 is_csl=4
+title=5
+is_number=6
+has_number=7
+date_indicator=8
 
 class NE_candidate:
     """A simple NE_candidate class"""
     
-    def __init__(self, phrase):
+    def __init__(self, phrase, position):
+        length=0
         global capitalized
         global start_of_sentence
         global abbreviation
-        length=0
         global all_capitalized
+        global is_csl
+        global title
+        global is_number
+        global has_number
+        global date_indicator
         '''other features:
                 POS_isAdjective
                 POS_endwithAdjective
                 Title
         '''
         self.phraseText=phrase
+        self.position=position
+        self.date_num_holder=[]
         self.length=len(phrase.split())
-        self.features = [None]*5
+        self.features = [None]*9
         return
     
     
@@ -33,18 +44,12 @@ class NE_candidate:
         return
     
     def print_obj(self):
-        print self.phraseText+" "+str(self.length),
+        print (self.phraseText+" "+str(self.length)+" "+str(self.position)+" "+str(self.date_num_holder), end=" ")
+        #print self.phraseText+" "+str(self.length),
         for feature in self.features:
-            print feature,
-        print ""
+            print (feature, end=" ")
+        print ("")
         return
 
 
-def main():
-
-    my_obj = NE_candidate("sample",1)
-    for i in range(5):
-        my_obj.set_feature(i,True)
-    my_obj.print_obj()
-    print my_obj.features[capitalized]
 
